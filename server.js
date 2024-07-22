6b6b8ad85f00ab83865fafe858b64b46eb60f57f
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-07-22 13:27:52 +0300

    Update Server.js | setup body-parser, method-override

--- server.js
+++ server.js
@@ -2,15 +2,24 @@
 const dotenv = require("dotenv").config(); // Loads the environment variables from .env file
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 
 //Database
 require('./config/database')
 
-  // landing page
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// landing page
   app.get("/", async (req, res, next) => {
     res.render("index.ejs");
   });
+  
 // port set up to port = 3000
   app.listen(3000, () => {
     console.log("Listening on port 3000");
