3576127b2b4cc1b8b6c8db330b03dbf2c991de0a
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-07-22 14:20:40 +0300

    Update Mongoose Connect

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.Database_URI)
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
